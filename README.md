fa18588426e9277cb4bef08caf1627a4c127a1e0
shamanth0611 <shamanth0611@gmail.com>
2025-02-13 11:40:24 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # StudentApplication
+# StudentApplication
---
+++ src/main/java/studentApp/Student.java
@@ -0,0 +1,11 @@
+package studentApp;
+
+public class Student {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
